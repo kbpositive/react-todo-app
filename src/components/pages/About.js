1c73e8625875d39fa4049e22e7850da79c905dfc
Kadeem Baldwin <kadbaldwin@gmail.com>
2020-11-29 21:35:43 -0600

    React Todo List app
    Completed Todo List app from Traversy Media tutorial; pending improvements

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+	return (
+		<React.Fragment>
+      <h1>About</h1>
+      <p>This Todo List app (v1.0.0) is a test app made using React.</p> 
+    </React.Fragment>
+	)
+}
+
+export default About;
